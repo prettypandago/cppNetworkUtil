df96cc27cc99dc8f4b4461743cd5cb3aa89e9db2
prettypandago <prettypandago@outlook.com>
2025-09-27 17:14:12 +0800

    1. Include "winsock2.h" in defines.h

--- include/defines.h
+++ include/defines.h
@@ -4,6 +4,10 @@
 #include <string>
 #include <vector>
 
+#ifdef _WIN32
+#include <winsock2.h>
+#endif
+
 // Adjust the macro definition here
 // #define IS_DEBUG // Enable debug mode
 #define BUFFERSIZE 4096
